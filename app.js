8f090430a32c55cae1515bb3f4b16eca766c4888
Ubuntu <ubuntu@ip-172-31-11-127.ap-south-1.compute.internal>
2026-03-10 18:31:54 +0000

    added app.js and jenkinsfile

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Students! CI/CD Pipeline Deployed This App 🚀");
+});
+
+app.get("/version", (req, res) => {
+  res.send("Version 1.0");
+});
+
+app.listen(PORT, () => {
+  console.log(Server running on port ${PORT});
+});
